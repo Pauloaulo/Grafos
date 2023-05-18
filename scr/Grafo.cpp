0e28b3a68d5c2a0449cd32c600325820a09d4c81
Paulo <65320589+Pauloaulo@users.noreply.github.com>
2023-05-17 23:17:21 -0300

    Add files via upload

--- scr/Grafo.cpp
+++ scr/Grafo.cpp
@@ -11,58 +11,105 @@ Grafo::Grafo(int n) {
 }
 
 int Grafo::num_comp_desconexas() {
+    bfs(0);
     int n_comp = 0;
-    for (int p : pai) {
+    for (int* p : pai) {
         if (p == NULL) n_comp += 1;
     }
-    return n_comp;
+    return n_comp - 1;
 }
 
 void Grafo::bfs(int fonte) {
     cor[fonte] = CINZA;
     dist[fonte] = 0;
-    std::queue<int> lista;
+    queue<int> lista;
     lista.push(fonte);
 
     while (lista.empty() == false) {
         int u = lista.front();
+        int* novo_pai = new int(u);
         lista.pop();
         for (int v : lista_adj[u]) {
             if (cor[v] == BRANCO) {
                 cor[v] = CINZA;
                 dist[v] = dist[u] + 1;
-                pai[v]  = u;
+                pai[v] = novo_pai;
                 lista.push(v);
             }
         } 
         cor[u] = PRETO;
     } 
        
-        for (int d : dist) cout << dist[d] << ", ";
-        cout << endl;
-        for (int p : pai) cout << pai[p] << ", ";
-        cout << endl;
+    //for (int d : dist) cout << d << ", ";
+    //cout << endl;
+    /*
+    for (int* p : pai) {
+        if (p != NULL) {
+            cout << *p << ", ";
+        } else cout << "NULL" << ", ";
+    }
+    cout << endl;
+    */
 }
 
 void Grafo::dfs() {
-    std::stack<int> pilha;
+    stack<int> pilha;
     cor[0] = CINZA;
     pilha.push(0);
     while(pilha.empty() == false) {
         int u = pilha.top();
+        int* novo_pai = new int(u);
         pilha.pop();
-        for(int v = lista_adj[u].size() - 1; v >= 0; v--) {
-            if(cor[v] == BRANCO) {
-                pai[v] = u;
-                cor[v] == CINZA;
-                pilha.push(v);
+        for(auto v = lista_adj[u].rbegin(); v != lista_adj[u].rend(); v++) {
+            int vi = *v;
+            if(cor[vi] == BRANCO) {
+                pai[vi] = novo_pai;
+                cor[vi] = CINZA;
+                pilha.push(vi);
             } 
         }
-        cor[u] == PRETO;
+        cor[u] = PRETO;
     }
 
-    for  (int p : pai) cout << pai[p] << ", ";
     cout << endl;
+    for (int* p : pai) {
+        if (p != NULL) {
+            cout << *p << ", ";
+        } else cout << "NULL" << ", ";
+    }
+    cout << endl;
+}
+
+void Grafo::menor_caminho(int fonte, int destino) {
+    if (fonte == destino) return; // ponto de partida e saída são iguais 
+    queue<int> caminho;
+
+    bfs(destino);
+
+    if (pai[fonte] == NULL) { // vertices desconexos 
+        cout << "Os vértices são desconexos." << endl;
+        return;
+    } 
+
+    const int* ponteiro = pai[fonte];
+    caminho.push(*ponteiro);
+    int u = caminho.front();
+    while (u != destino) {
+
+        const int* aux = pai[u];
+        caminho.push(*aux);
+        u = *aux;
+    }
+
+    cout << "{";
+    while (!caminho.empty()) {
+        int elemento = caminho.front();
+        caminho.pop();
+        if (caminho.size() != 0) {
+            cout << elemento << ", ";
+        } else cout << elemento;
+    }
+    cout << "}" << endl;
 }
 
 void Grafo::setMatriz_adj(int i, int j, int num) {
@@ -73,8 +120,8 @@ int Grafo::getMatriz_adj(int i, int j) {
     return matriz_adj[i][j];
 }
 
-void Grafo::setLIsta_adj(int i, int j, int num) {
-    lista_adj[i][j] = num;
+void Grafo::setLista_adj(int i, int num) {
+    lista_adj[i].push_back(num);
 }
 
 int Grafo::getLista_adj(int i, int j) {
--- scr/main.cpp
+++ scr/main.cpp
@@ -3,20 +3,30 @@
 #include <string>
 #include <sstream>
 
-Grafo load_from(const std::string& nome_arquivo);
+Grafo* load_from(const std::string& nome_arquivo);
 
 int main() {
+    // Todos os testes são com grafos de 50 vértices
+    // Se desejar testar com outros grafos é só mudar o nome do arquivo em load_from
+    Grafo* gr = load_from("pcv50.txt");
+    
+    //gr->bfs(0);
+    //gr->dfs();
+    //gr->menor_caminho(32, 4);
+    //cout << gr->num_comp_desconexas() << endl;
 
     return 0;
 }
 
-Grafo load_from(const std::string& nome_arquivo) {
+Grafo* load_from(const std::string& nome_arquivo) {
     std::ifstream arquivo(nome_arquivo);
     std::string linha;
-    int n;
+    int n = 0;
     while(std::getline(arquivo, linha)) {
         n++;
     }
+    arquivo.clear();
+    arquivo.seekg(0, std::ios::beg);
 
     Grafo* g = new Grafo(n);
 
@@ -35,7 +45,11 @@ Grafo load_from(const std::string& nome_arquivo) {
             int i = numeros[_id];
             g->setMatriz_adj(l, c, (int)i);
             if (g->getMatriz_adj(l, c) > 0) {
-                g->
+                g->setLista_adj(l, c);
             }
+            c += 1;
         }
+        l += 1;
+    }
+    return g;
 }
